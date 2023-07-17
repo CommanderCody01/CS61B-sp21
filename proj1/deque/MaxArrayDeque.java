b09974cb4afa6ccd1d8d0c6030df972fb3bf3e2a
CommanderCody01 <qy624@nyu.edu>
2023-07-17 18:32:44 +0800

    first part4

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -2,47 +2,64 @@ package deque;
 import java.util.Comparator;
 
 public class MaxArrayDeque<T> extends ArrayDeque<T> {
-    private Comparator<T> com;
+    private Comparator<T> comparator;
 
     public MaxArrayDeque(Comparator<T> c) {
         super();
-        com = c;
+        comparator = c;
+    }
+
+//    public T max() {
+//        if (this.isEmpty()) {
+//            return null;
+//        }
+//        int maxIndex = this.getFrontIndex();
+//        int curr = this.getFrontIndex();
+//        int count = 0;
+//        while (count < this.size()) {
+//            int cmp = com.compare(this.get(curr), this.get(maxIndex));
+//            if (cmp > 0) {
+//                maxIndex = curr;
+//            }
+//            curr = nextIndex(curr);
+//            count += 1;
+//        }
+//        return this.get(maxIndex);
+//    }
+//
+//    public T max(Comparator<T> c) {
+//        if (this.isEmpty()) {
+//            return null;
+//        }
+//        int maxIndex = this.getFrontIndex();
+//        int curr = this.getFrontIndex();
+//        int count = 0;
+//        while (count < this.size()) {
+//            int cmp = c.compare(this.get(curr), this.get(maxIndex));
+//            if (cmp > 0) {
+//                maxIndex = curr;
+//            }
+//            curr = nextIndex(curr);
+//            count += 1;
+//        }
+//        return this.get(maxIndex);
+//    }
+
+    public T max(Comparator<T> c) {
+        if (isEmpty()) {
+            return null;
+        }
+        int maxIndex = 0;
+        for (int i = 1; i < size(); i++) {
+            if (c.compare(get(i), get(maxIndex)) > 0) {
+                maxIndex = i;
+            }
+        }
+        return get(maxIndex);
     }
 
     public T max() {
-        if (this.isEmpty()) {
-            return null;
-        }
-        int maxIndex = this.getFrontIndex();
-        int curr = this.getFrontIndex();
-        int count = 0;
-        while (count < this.size()) {
-            int cmp = com.compare(this.get(curr), this.get(maxIndex));
-            if (cmp > 0) {
-                maxIndex = curr;
-            }
-            curr = nextIndex(curr);
-            count += 1;
-        }
-        return this.get(maxIndex);
-    }
-
-    public T max(Comparator<T> c) {
-        if (this.isEmpty()) {
-            return null;
-        }
-        int maxIndex = this.getFrontIndex();
-        int curr = this.getFrontIndex();
-        int count = 0;
-        while (count < this.size()) {
-            int cmp = c.compare(this.get(curr), this.get(maxIndex));
-            if (cmp > 0) {
-                maxIndex = curr;
-            }
-            curr = nextIndex(curr);
-            count += 1;
-        }
-        return this.get(maxIndex);
+        return max(comparator);
     }
 
 }
