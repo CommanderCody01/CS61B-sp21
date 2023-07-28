e2b0ae6d52d708b9a1533f96d391b10ff6252add
CommanderCody01 <qy624@nyu.edu>
2023-07-28 13:51:54 +0800

    lab7 over

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -1,9 +1,9 @@
 package bstmap;
 
 
-import java.util.Iterator;
-import java.util.Set;
-import java.util.Spliterator;
+import jh61b.junit.In;
+
+import java.util.*;
 import java.util.function.Consumer;
 
 public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
@@ -114,30 +114,204 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         }
     }
 
+    private void inOrderHelper(Node root) {    // L D R
+        if (root == null) {
+            return;
+        }
+        inOrderHelper(root.left);
+        System.out.print(root.key + " ");
+        inOrderHelper(root.right);
+    }
     public void printInOrder() {
-        return;
+        inOrderHelper(this.root);
+        System.out.println();
     }
 
     @Override
     public Set<K> keySet() {
-        return null;
+        Set<K> ret = new HashSet<>();
+        for (K ele : this) {
+            ret.add(ele);
+        }
+        return ret;
     }
+
+    public Node minOfRight(Node root) {
+        Node curr = root;
+        while (curr.left != null && curr.right != null) {      // to make curr a leaf Node
+            curr = curr.left;
+        }
+        return curr;
+    }
+
+    private int numOfChild(Node root) {
+        int count = 0;
+        if (root.left != null ) {
+            count += 1;
+        }
+        if (root.right != null) {
+            count += 1;
+        }
+        return count;
+    }
+
+    public Node findParent(Node node) {
+        Node curr = root;
+        Node prev = curr;
+        if (node.key.equals(root.key)) {
+            return null;
+        }
+        while ( !(curr.key.equals(node.key)) ) {
+            prev = curr;
+            int cmp = node.key.compareTo(curr.key);
+            if (cmp > 0 ) {        // node > curr
+                curr = curr.right;
+            }
+            else if (cmp < 0) {
+                curr = curr.left;
+            }
+        }
+        return prev;
+    }
+
+    public Node delete(Node node) {
+        int numChild = numOfChild(node);
+        if (numChild == 0) {
+            if (size == 1) {        // 这是root
+                size = 0;
+                root = null;
+                return node;
+            }
+            else {                  // leaf node
+                Node parent = findParent(node);
+                if (parent.left != null && parent.left.key.equals(node.key)) {
+                    parent.left = null;
+                    size -= 1;
+                    return node;
+                }
+                else {
+                    parent.right = null;
+                    size -= 1;
+                    return node;
+                }
+            }
+        }
+        else if (numChild == 1) {
+            Node parent = findParent(node);
+            if (parent == null) {    // node 是 root
+                if (node.left != null) {
+                    root = root.left;
+                }
+                else {
+                    root = root.right;
+                }
+                size -= 1;
+                return node;
+            }
+            if (parent.left != null && parent.left.key.equals(node.key)) {
+                if (node.left != null) {    // node只有.left
+                    parent.left = node.left;
+                    size -= 1;
+                    return node;
+                }
+                else {
+                    parent.left = node.right;
+                    size -= 1;
+                    return node;
+                }
+            }
+            else {
+                if (node.left != null) {    // node只有.left
+                    parent.right = node.left;
+                    size -= 1;
+                    return node;
+                }
+                else {
+                    parent.right = node.right;
+                    size -= 1;
+                    return node;
+                }
+            }
+        }
+        else {
+            Node successor = minOfRight(node);
+            node.key = successor.key;
+            node.value = successor.value;
+            delete(successor);
+//            size -= 1;
+            return node;
+        }
+    }
+
+
 
     @Override
     public V remove(K key) {
-        return null;
+        Node findN = find(key);
+        if (findN == null) {
+            return null;
+        }
+        else {
+            V val = findN.value;
+            delete(findN);
+            return val;
+        }
     }
 
     @Override
     public V remove(K key, V value) {
-        throw new UnsupportedOperationException();
+//        throw new UnsupportedOperationException();
 //        return null;
+        Node findN = find(key);
+        if (findN == null || !(findN.value.equals(value)) ) {
+            return null;
+        }
+        else {
+            V val = findN.value;
+            delete(findN);
+            return val;
+        }
     }
 
+    public class treeInterator implements Iterator<K> {
+        private List<K> data;
+        private int index;
+
+        private void helper(List<K> lst, Node root) {
+            if (root == null) {
+                return;
+            }
+            helper(lst, root.left);
+            lst.add(root.key);
+            helper(lst, root.right);
+        }
+        public List<K> inorderTraversal(Node root) {
+            List<K> lst = new ArrayList<>();
+            helper(lst, root);
+            return lst;
+        }
+        public treeInterator() {
+            data = inorderTraversal(root);
+            index = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return index < data.size();
+        }
+
+        @Override
+        public K next() {
+            K ret = data.get(index);
+            index += 1;
+            return ret;
+        }
+    }
     @Override
     public Iterator<K> iterator() {
-        throw new UnsupportedOperationException();
-//        return null;
+//        throw new UnsupportedOperationException();
+        return new treeInterator();
+
     }
 
     @Override
--- lab7/bstmap/Test.java
+++ lab7/bstmap/Test.java
@@ -5,12 +5,31 @@ import static org.junit.Assert.assertTrue;
 public class Test {
 
     public static void main(String[] args) {
-        BSTMap<String, Integer> b = new BSTMap<String, Integer>();
-        for (int i = 0; i < 11; i++){
-            String x = "hi" + i;
-//            System.out.println(x);
-            b.put("hi" + i, 1 + i);
+//        BSTMap<Integer, Integer> b = new BSTMap<Integer, Integer>();
+//        for (int i = 0; i < 11; i++){
+//            b.put(i, 1 + i);
+//        }
+//
+//        b.printInOrder();
+//
+//        BSTMap<String,String> q = new BSTMap<String,String>();
+//        q.put("c","a");
+//        q.put("b","a");
+//        q.put("a","a");
+//        q.put("d","a");
+//        q.put("e","a");
+//        q.printInOrder();
+//        q.remove("e");
+//        q.printInOrder();
+        BSTMap rightChild = new BSTMap();
+        rightChild.put('A', 1);
+        rightChild.put('B', 2);
+        Integer result = (Integer) rightChild.remove('A');
+        for (int i = 0; i < 10; i++) {
+            rightChild.put((char) ('C'+i), 3+i);
         }
+        rightChild.put('A', 100);
+        rightChild.printInOrder();
 
     }
 
